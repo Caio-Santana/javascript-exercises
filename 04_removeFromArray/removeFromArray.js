13c259e8d61b79bbbdf4b913f218487aa78c8523
Caio-Santana <caio.santana@live.com>
2022-07-14 10:35:24 -0300

    Done exercise 04-reverseString

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function () {
+    let numberOfArgs = arguments.length;
+    let argsArray = Array.from(arguments);
+    const array = argsArray[0];
+    for (let i = 1; i < numberOfArgs; i++) {
+        let index = array.indexOf(argsArray[i]);
+        if (index < 0) {
+            continue;
+        }
+        if (typeof (argsArray[i]) === typeof (array[index])) {
+            array.splice(index, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
