a7f995025dc8bdca6e489634b076486d9a1327d8
Caio-Santana <caio.santana@live.com>
2022-07-14 11:35:18 -0300

    Done exercise 05-sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (firstArg, secondArg) {
+    const notAInteger = !Number.isInteger(firstArg) || !Number.isInteger(secondArg);
+    const negative = (firstArg < 0) || (secondArg < 0);
+    if (notAInteger || negative) {
+        return 'ERROR';
+    }
 
+    let start = firstArg,
+        end = secondArg;
+    if (firstArg > secondArg) {
+        start = secondArg;
+        end = firstArg;
+    }
+
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
