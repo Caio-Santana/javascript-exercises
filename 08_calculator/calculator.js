84010b0dd7c3f6ba93ddcf10ce92d62ee259bd3a
Caio-Santana <caio.santana@live.com>
2022-07-20 11:06:25 -0300

    Done exercise 08-calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function(a, b) {
+  if (arguments.length === 0) {
+    return 0;
+  }
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  if (arguments.length === 0) {
+    return 0;
+  }
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	if (array.length === 0) {
+    return 0;
+  }
+
+  let result = 0;
+  array.forEach(n => result += n);
+  return result;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+	if (array.length === 0) {
+    return 0;
+  }
 
+  let result = 1;
+  array.forEach(n => result *= n);
+  return result;
 };
 
-const power = function() {
+const power = function(a, b) {
+  if (arguments.length === 0) {
+    return 0;
+  }
 	
+  return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0) {
+    return 1;
+  }
+
+  return factorial(n - 1) * n;
 };
 
 // Do not edit below this line
