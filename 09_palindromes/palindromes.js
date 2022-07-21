5e145e82a619bbae59446df3ef9e05003a1a9869
Caio-Santana <caio.santana@live.com>
2022-07-21 08:09:26 -0300

    Done exercise 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const regex = /[a-z]/;
 
-};
+  const original = str.toLowerCase()
+    .split("")
+    .filter((x) => x.match(regex));
 
+  const palindrome = original.slice();
+
+  palindrome.reverse();
+  
+  return original.toString() === palindrome.toString();
+}
 // Do not edit below this line
 module.exports = palindromes;
