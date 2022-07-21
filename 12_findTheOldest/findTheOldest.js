f8ad58449a1fa2f9fe68e5896baa112c00b1c636
Caio-Santana <caio.santana@live.com>
2022-07-21 09:58:29 -0300

    Done exercise 12-findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  
+  return people.reduce(
+    (oldest, person) => {
+      const age =
+        "yearOfDeath" in person
+          ? person.yearOfDeath - person.yearOfBirth
+          : new Date().getFullYear() - person.yearOfBirth;
+  
+      if (oldest.age < age) {
+        oldest.name = person.name;
+        oldest.age = age;
+      }
 
+      return oldest;
+    },
+    { name: "", age: 0 }
+  );
 };
 
 // Do not edit below this line
