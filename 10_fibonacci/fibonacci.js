f81c401a6b1756fb81eafb206ed01e241d8f2b6e
Caio-Santana <caio.santana@live.com>
2022-07-21 08:42:06 -0300

    Done exercise 10-fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS";
+    }
 
+    if (n === 0) {
+        return 0;
+    }
+
+    let pp = 1,
+        previous = 0,
+        result = 0;
+    for (let i = 0; i < n; i++) {
+        result = pp + previous;
+        pp = previous;
+        previous = result;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
